2dc66f92d861ef8594f899d8fd07c893ee3a7a9a
Ma7moudGh25 <131613471+Ma7moudGh25@users.noreply.github.com>
2024-02-26 17:26:45 +0200

    Update Source.cpp

--- Bank Management System/Bank Management System/Source.cpp
+++ Bank Management System/Bank Management System/Source.cpp
@@ -20,7 +20,8 @@ int main()
 		cout << "Press" << endl;
 		cout << "1-To open an account" << endl;
 		cout << "2-To do bank transactions with your account" << endl;
-		cout << "3-To exit" << endl;
+		cout << "3-To edit or delete an account" << endl;
+		cout << "4-To exit" << endl;
 		cin >> select;
 		if (select == '1')
 		{
@@ -39,10 +40,15 @@ int main()
 				cin >> money_amount;
 				cout << "Create a password for your account" << endl;
 				cin >> pass;
-				p[pass].name = name;
-				p[pass].n_id = n_id;
-				p[pass].money_amount = money_amount;
-				p[pass].pass = pass;
+				if (p[pass].pass == pass)
+					cout << "This password has an account" << endl;
+				else
+				{
+					p[pass].name = name;
+					p[pass].n_id = n_id;
+					p[pass].money_amount = money_amount;
+					p[pass].pass = pass;
+				}
 			}
 			else if (choice == '2')
 			{
@@ -79,7 +85,7 @@ int main()
 				}
 				else
 				{
-					cout << "Wrong password" << endl;
+					cout << "There is no account with this password" << endl;
 					continue;
 				}
 			}
@@ -98,7 +104,7 @@ int main()
 				}
 				else
 				{
-					cout << "Wrong password" << endl;
+					cout << "There is no account with this password" << endl;
 					continue;
 				}
 			}
@@ -115,13 +121,134 @@ int main()
 				}
 				else
 				{
-					cout << "Wrong password" << endl;
+					cout << "There is no account with this password" << endl;
 					continue;
 				}
 			}
 		}
+		else if (select == '3')
+		{
+			char choice;
+			cout << "Press" << endl;
+			cout << "1-To enter with password" << endl;
+			cout << "2-To enter with name and national ID" << endl;
+			cout << "3-To back to main menu" << endl;
+			cin >> choice;
+			if (choice == '1')
+			{
+				cout << "Enter your password to edit or delete the account" << endl;
+				cin >> pass;
+				int pass1 = pass;
+				if (pass == p[pass].pass)
+				{
+					char dec;
+					cout << "Press" << endl;
+					cout << "1-To edit your account" << endl;
+					cout << "2-To delete your account" << endl;
+					cout << "3-To back to main menu" << endl;
+					cin >> dec;
+					if (dec == '1')
+					{
+						cout << "Enter a new name" << endl;
+						cin >> name;
+						cout << "Enter a new national ID" << endl;
+						cin >> n_id;
+						cout << "Enter a new amount of money" << endl;
+						cin >> money_amount;
+						cout << "Enter a new password" << endl;
+						cin >> pass;
+						if (pass != pass1 && pass == p[pass].pass)
+						{
+							cout << "This password has an account" << endl;
+						}
+						else
+						{
+							p[pass].name = name;
+							p[pass].n_id = n_id;
+							p[pass].money_amount = money_amount;
+							p[pass].pass = pass;
+							cout << "The account info changed" << endl;
+						}
+					}
+					else if (dec == '2')
+					{
+						p[pass].name = -1;
+						p[pass].n_id = -1;
+						p[pass].money_amount = -1;
+						p[pass].pass = -1;
+						cout << "Your account has been deleted" << endl;
+					}
+				}
+				else
+				{
+					cout << "There is no account with this password" << endl;
+					continue;
+				}
+			}
+			else if (choice == '2')
+			{
+				cout << "Enter your name and national ID to edit or delete your account" << endl;
+				cin >> name>>n_id;
+				bool found = 0;
+				for (int i = 0; i < 10000; i++)
+				{
+					if (name == p[i].name && n_id == p[i].n_id)
+					{
+						found = 1;
+						break;
+					}
+				}
+				if (found)
+				{
+					char dec;
+					cout << "Press" << endl;
+					cout << "1-To edit your account" << endl;
+					cout << "2-To delete your account" << endl;
+					cout << "3-To back to main menu" << endl;
+					cin >> dec;
+					if (dec == '1')
+					{
+						cout << "Enter a new name" << endl;
+						cin >> name;
+						cout << "Enter a new national ID" << endl;
+						cin >> n_id;
+						cout << "Enter a new amount of money" << endl;
+						cin >> money_amount;
+						cout << "Enter a new password" << endl;
+						cin >> pass;
+						if (pass == p[pass].pass)
+						{
+							cout << "This password has an account" << endl;
+							continue;
+						}
+						else
+						{
+							p[pass].name = name;
+							p[pass].n_id = n_id;
+							p[pass].money_amount = money_amount;
+							p[pass].pass = pass;
+							cout << "The account info changed" << endl;
+						}
+					}
+					else if (dec == '2')
+					{
+						p[pass].name = -1;
+						p[pass].n_id = -1;
+						p[pass].money_amount = -1;
+						p[pass].pass = -1;
+						cout << "Your account has been deleted" << endl;
+					}
+				}
+				else
+				{
+					cout << "There is no account with these info" << endl;
+					continue;
+				}
+				
+			}
+		}
 		cout << "--------------------------------------------" << endl;
-	} while (select != '3');
+	} while (select != '4');
 	
 
 	return 0;
